75c537f9fbbe800c45b35d69654f009432050ac1
亚信-许晓东 <xuxd@asiainfo.com>
2018-07-16 18:40:17 +0800

    工程基本界面结构

---
+++ src/main/java/com/xuxd/mysql/console/Application.java
@@ -0,0 +1,18 @@
+package com.xuxd.mysql.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: 许晓东
+ * @Date: 18-7-14 21:45
+ * @Description:
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/resources/public/js/custom/app.js
@@ -0,0 +1,87 @@
+function addMysqlConnectioinInfo() {
+
+    var name = $("#db_source_add_info.name").val();
+    var mysql_ip = $("#db_source_add_info.mysql_ip").val();
+    var mysql_port = $("#db_source_add_info.mysql_port").val();
+    var mysql_pass = $("#db_source_add_info.mysql_pass").val();
+    var name = $("#db_source_add_info.name").innerText;
+
+    if (!isIp(mysql_ip) || !isNum(mysql_port)) return;
+    if (isEmptyStr(name)) {
+        name = mysql_ip + "_" + mysql_port;
+    }
+    var data = {"name": name, "ip": mysql_ip, "port": mysql_port, "pass": mysql_pass};
+    alert(data);
+    $("#db_source_add_model").modal('toggle');
+}
+
+/**
+ * 给表单元素增加不同效果
+ * @param e
+ * @param reg
+ * @param v
+ */
+function addClassIfTestTrue(e, condition) {
+    if (!condition) {
+        if (e.hasClass("has-success")) {
+            e.removeClass("has-success");
+        }
+        if (!e.hasClass("has-error")) {
+            e.addClass("has-error");
+        }
+        return false;
+    } else {
+        if (e.hasClass("has-error")) {
+            e.removeClass("has-error");
+        }
+        if (!e.hasClass("has-success")) {
+            e.addClass("has-success");
+        }
+        return true;
+    }
+}
+
+function isIp(ip) {
+    var reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+    return reg.test(ip);
+}
+
+function isNum(num) {
+    var reg = /^[1-9][0-9]*$/;
+    return reg.test(num);
+}
+
+/**
+ * 校验IP
+ * @param e
+ */
+function validateIp(e) {
+    var grandfather = $(e).parent().parent();
+    addClassIfTestTrue(grandfather, isIp(e.value));
+}
+
+/**
+ * 是否是数字
+ * @param e
+ */
+function validatePort(e) {
+    var grandfather = $(e).parent().parent();
+    addClassIfTestTrue(grandfather, isNum(e.value));
+}
+
+function isEmptyStr(str) {
+
+}
+
+function nullShowError(e) {
+    var v = e.value;
+    var grandfather = $(e).parent().parent();
+    if (v == null || v == "") {
+        grandfather.addClass("has-error");
+    } else {
+        if (grandfather.hasClass("has-error")) {
+            grandfather.removeClass("has-error");
+            grandfather.addClass("has-success");
+        }
+    }
+}
